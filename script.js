411ed99d66d57b48e609f56f233e3c26aa74ab22
Shriyansh Lohia <132228919+whoshriyansh@users.noreply.github.com>
2023-06-18 17:46:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+const API_KEY = `3265874a2c77ae4a04bb96236a642d2f`
+const form = document.querySelector("form")
+const search = document.querySelector("#search")
+const weather = document.querySelector('#weather')
+
+
+
+// const API = `https://api.openweathermap.org/data/2.5/weather?
+// q=${city}&appid=${API_KEY}&units=metric`
+// const IMG_URL = `https: //openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+
+
+const getWeather = async (city) => {
+    weather.innerHTML = `<h1> Loading....</h1>`
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+    const response = await fetch(url);
+    const data = await response.json();
+    return showWeather(data);
+}
+
+
+const showWeather = (data) => {
+
+    if (data.cod == "404") {
+        weather.innerHTML = `<h1> City not found </h1>`
+        return;
+    }
+    weather.innerHTML = `
+    <div>
+    <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="">
+    </div>
+     <div>
+        <h1>${data.main.temp}degree</h1>
+        <h5>${data.weather[0].main}  </h5>
+     </div> `
+}
+
+
+form.addEventListener(
+    'submit',
+    function (event) {
+        getWeather(search.value)
+        event.preventDefault()
+    }
+)
